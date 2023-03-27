e6be59e95431be16dab29c23a10d551604879ffa
aplloshahriar <aaronshahriar.as@gmail.com>
2023-03-27 12:27:46 +0600

    style added

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import '../product/Product.css';
 
 const Product = (props) => {
-    const {img,name,seller,price,quantity}=props.product;
+    const { img, name, seller, price, ratings } = props.product;
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h6>{name}</h6>
-       
+            <div className='product-info'>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price:${price}</p>
+                <p>Manufacturer{seller}</p>
+                <p>Ratings:{ratings} star</p>
+            </div>
+            <button className='btn-cart'>Add To Cart</button>
+
         </div>
     );
 };
