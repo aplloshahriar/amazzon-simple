c11ab465b2b08e2dcfd0c83d69fe7080973d79d0
aplloshahriar <aaronshahriar.as@gmail.com>
2023-03-27 11:12:43 +0600

    load data

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    const [products,setProducts]=useState([]);
+    useEffect(()=>{
+            fetch('products.json')
+            .then(res=>res.json())
+            .then(data =>setProducts(data))
+    },[])
+
+    return (
+        <div className='shop-container'>
+
+            <div className="products-container">
+        <h2>Product: {products.length}</h2>
+            </div>
+
+            <div className="card-container">
+        <h4>Order</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
