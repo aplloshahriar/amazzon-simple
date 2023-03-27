6079d632bea41ae4a042777e1014a27a8cdfe1da
aplloshahriar <aaronshahriar.as@gmail.com>
2023-03-27 20:58:15 +0600

    order summery working

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,6 +4,9 @@ import './Shop.css';
 
 const Shop = () => {
     const [products,setProducts]=useState([]);
+
+    const [cart,setCart]= useState([])
+
     useEffect(()=>{
             fetch('products.json')
             .then(res=>res.json())
@@ -11,7 +14,10 @@ const Shop = () => {
     },[]);
 
     const handleAddToCart=(product)=>{
-        console.log(product)
+        // console.log(product);
+        const newCart=[...cart,product];
+        setCart(newCart);
+
     }
 
     return (
@@ -28,7 +34,8 @@ const Shop = () => {
             </div>
 
             <div className="card-container">
-        <h4>Order</h4>
+        <h4>Order Summery</h4>
+        <p>Selected Items: {cart.length}</p>
             </div>
         </div>
     );
