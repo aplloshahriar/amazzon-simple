ede194c396a40e8e5ae481f4cca6aca1d6fbb583
aplloshahriar <aaronshahriar.as@gmail.com>
2023-03-29 11:26:09 +0600

    id saved in local storage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -5,25 +5,44 @@ import Product from '../product/Product';
 import './Shop.css';
 
 const Shop = () => {
-    const [products,setProducts]=useState([]);
+    const [products, setProducts] = useState([]);
 
-    const [cart,setCart]= useState([])
+    const [cart, setCart] = useState([])
 
-    useEffect(()=>{
-            fetch('products.json')
-            .then(res=>res.json())
-            .then(data =>setProducts(data))
-    },[]);
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
 
 
-    useEffect(()=>{
-        const storedCart=getShoppingCart()
-        console.log(storedCart);
-    },[])
+    useEffect(() => {
+        const storedCart = getShoppingCart()
+        // console.log(storedCart);
+        const savedCart=[];
 
-    const handleAddToCart=(product)=>{
+        // step-1 get id
+        for (const id in storedCart) {
+
+            //step-2 get the product by using id
+            const addedProduct = products.find(product => product.id === id)
+            // console.log(savedProducts);
+            if (addedProduct) {
+                // step-3 get the quantity of the product
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+
+                // step-4 add the addedProduct to the saved cart
+                savedCart.push(addedProduct);
+            }
+        }
+        // step-5 set the cart
+        setCart(savedCart);
+    }, [products])
+
+    const handleAddToCart = (product) => {
         // console.log(product);
-        const newCart=[...cart,product];
+        const newCart = [...cart, product];
         setCart(newCart);
         addToDb(product.id)
     }
@@ -32,14 +51,14 @@ const Shop = () => {
         <div className='shop-container'>
 
             <div className="products-container">
-            {
-                products.map(product=><Product
-                 key={product.id}
-                 product={product}
-                 handleAddToCart={handleAddToCart}
-                 
-                 ></Product>)
-            }
+                {
+                    products.map(product => <Product
+                        key={product.id}
+                        product={product}
+                        handleAddToCart={handleAddToCart}
+
+                    ></Product>)
+                }
             </div>
 
             <div className="card-container">
--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import './Cart.css';
 
-const Cart = ({cart}) => {
+const Cart = ({ cart }) => {
     // const cart=props.cart; option-1
-   // const {cart}=props; // option-2
-   let total=0;
-   let totalShipping=0
-    for(const product of cart){
-        total=total+product.price;
-        totalShipping=totalShipping+product.shipping;
+    // const {cart}=props; // option-2
+    let total = 0;
+    let totalShipping = 0;
+    let quantity = 0;
+
+    for (const product of cart) {
+        product.quantity=product.quantity || 1;
+        total = total + product.price *product.quantity;
+        totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
-    const tax=total*7/100;
-    const grandTotal=total+totalShipping+tax;
-   
+    const tax = total * 7 / 100;
+    const grandTotal = total + totalShipping + tax;
+
     return (
         <div className='cart'>
-             <h4>Order Summery</h4>
-        <p>Selected Items: {cart.length}</p>
-        <p>Total Price: ${total}</p>
-        <p>Total SHipping: ${totalShipping} </p>
-        <p>Tax: {tax.toFixed(2)}</p>
-        <h5>Grand Total: {grandTotal.toFixed(2)} </h5>
+            <h4>Order Summery</h4>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: ${total}</p>
+            <p>Total SHipping: ${totalShipping} </p>
+            <p>Tax: {tax.toFixed(2)}</p>
+            <h5>Grand Total: {grandTotal.toFixed(2)} </h5>
         </div>
     );
 };
