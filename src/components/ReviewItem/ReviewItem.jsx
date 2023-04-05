f7fff0007a707d1e97290786857dc001a0ad901f
aplloshahriar <aaronshahriar.as@gmail.com>
2023-04-05 21:34:22 +0600

    implement delete from cart

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './ReviewItem.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
-const ReviewItem = ({product}) => {
+const ReviewItem = ({product,handleRemoveFromCart}) => {
     const {id,img,name,price,quantity}=product;
     return (
         <div className='review-item'>
             <img src={img} alt="" />
+            <div className='review-details'>
+                <p className='product-title'>{name}</p>
+                <p>Price: <span className='orange-text'>${price}</span></p>
+                <p>Quantity: <span className='orange-text'>${quantity}</span></p>
+               
+            </div>
+            <button onClick={()=>handleRemoveFromCart(id)} className='btn-delete'> 
+            <FontAwesomeIcon className='delete-icon' icon={faTrashAlt} /></button>
         </div>
     );
 };
