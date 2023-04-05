cbdabd44a8950a9c7c0b2c7ac34199c5afc06623
aplloshahriar <aaronshahriar.as@gmail.com>
2023-04-05 21:53:04 +0600

    clear cart button added

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -3,7 +3,7 @@ import Cart from '../cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import ReviewItem from '../ReviewItem/ReviewItem';
 import './Orders.css'
-import { removeFromDb } from '../../utilities/fakedb';
+import { deleteShoppingCart, removeFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
     const savedCart=useLoaderData();
@@ -13,6 +13,12 @@ const Orders = () => {
         const remaining=cart.filter(product=>product.id !==id);
         setCart(remaining);
         removeFromDb(id);
+    }
+
+
+    const handleClearCart=()=>{
+        setCart([]);
+        deleteShoppingCart();
     }
 
     return (
@@ -28,7 +34,10 @@ const Orders = () => {
             </div>
 
             <div className='card-container'>
-                <Cart cart={cart}></Cart>
+                <Cart 
+                cart={cart}
+                handleClearCart={handleClearCart}
+                ></Cart>
             </div>
         </div>
     );
--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './Cart.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart,handleClearCart }) => {
     // const cart=props.cart; option-1
     // const {cart}=props; // option-2
     let total = 0;
@@ -25,6 +27,10 @@ const Cart = ({ cart }) => {
             <p>Total SHipping: ${totalShipping} </p>
             <p>Tax: {tax.toFixed(2)}</p>
             <h5>Grand Total: {grandTotal.toFixed(2)} </h5>
+            <button onClick={()=>handleClearCart()} className='btn-clear-cart'>
+                <span >Clear Cart</span>
+                <FontAwesomeIcon icon={faTrashAlt} />
+                </button>
         </div>
     );
 };
